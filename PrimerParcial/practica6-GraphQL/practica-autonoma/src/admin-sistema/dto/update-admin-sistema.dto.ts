442539fe9f21ac191a52dda6399d79b314eeef2f
David Jaramillo <djdavidjaramillo@gmail.com>
2025-10-21 14:57:45 -0500

    taller 6

--- PrimerParcial/practica6-GraphQL/practica-autonoma/src/admin-sistema/dto/update-admin-sistema.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAdminSistemaDto } from './create-admin-sistema.dto';
-
-export class UpdateAdminSistemaDto extends PartialType(CreateAdminSistemaDto) {}
